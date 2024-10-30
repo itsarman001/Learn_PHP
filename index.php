6ce0d38f8f139992728810ddc73c724e24887da0
Md Arman <armanar6712@gmail.com>
2024-10-30 23:04:21 +0530

    Simplify PHP topics table with dynamic PHP code and clean up HTML structure

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Topics</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>PHP Topics</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>SL No</th>
+                    <th>Topic</th>
+                    <th>Link</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $topics = [
+                    ["Variables", "./topics/variables.php"],
+                    ["Form Handling", "./topics/form_handling.php"],
+                    ["Operators", "./topics/operators.php"],
+                    ["Math Functions", "./topics/math_functions.php"],
+                    ["If Else Statements", "./topics/if_else.php"],
+                    ["Switch Case", "./topics/switch_case.php"],
+                    ["For Loop", "./topics/for_loop.php"],
+                    ["While Loop", "./topics/while_loop.php"],
+                    ["Arrays", "./topics/arrays.php"],
+                    ["Associative Arrays", "./topics/associative_arrays.php"],
+                    ["Isset and Empty", "./topics/isset_empty.php"],
+                    ["Radio Button", "./topics/radio_button.php"]
+                ];
+
+                foreach ($topics as $index => $topic) {
+                    echo "<tr>";
+                    echo "<td>" . ($index + 1) . "</td>";
+                    echo "<td><h4>" . $topic[0] . "</h4></td>";
+                    echo "<td><a href='" . $topic[1] . "'>Link</a></td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
